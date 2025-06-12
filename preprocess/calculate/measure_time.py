e24e041996debabd34b14427849c762018a97ad3
Kohsei Mochizuki <kose@KohseinoMacBook-Pro.local>
2025-06-13 01:28:03 +0900

    #xxx feat: 余分なコードの削除とgitignoreを追加・更新

--- preprocess/calculate/measure_time.py
+++ preprocess/calculate/measure_time.py
@@ -1,9 +1,7 @@
 from datetime import datetime
 
+# NOTE:ISO 8601フォーマットの文字列（小数秒あり）を引数にとる
 def calc_time_diff(start_time,end_time):
-    # ISO 8601フォーマットの文字列（小数秒あり）
-    start_time = "2025-05-31T01:46:07.680Z"
-    end_time = "2025-05-31T02:08:48.432Z"
 
     start_dt = datetime.fromisoformat(start_time.replace("Z", "+00:00"))
     end_dt = datetime.fromisoformat(end_time.replace("Z", "+00:00"))
