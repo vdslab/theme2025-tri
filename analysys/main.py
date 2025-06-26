edbb8cda6271267992d784fc79e27e332472c1d4
Kohsei Mochizuki <kose@KohseinoMacBook-Pro.local>
2025-06-27 04:04:25 +0900

    #27 feat: gamepkを入力して回帰データを取得する処理の実装

--- analysys/main.py
+++ analysys/main.py
@@ -12,3 +12,18 @@ from analysys.logistic_regression_analysis.Logistic_regression_analysis import L
 from preprocess.data_process_for_ra import data_process_for_ra
 from preprocess.data_processor import data_process
 
+def main():
+    # 777398
+    gamepk = input("Enter the gamepk: ")
+    
+    _,processed_data = data_process(gamepk)
+    
+    match_data = data_process_for_ra(processed_data,gamepk)
+        
+    molded_data = time_data_sellecting(gamepk,match_data)
+
+    Logistic_regression_analysis(gamepk,molded_data)
+
+
+if __name__ == "__main__":
+    main()
