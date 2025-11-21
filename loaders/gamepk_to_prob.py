dd8e620574706d8e917b69438396013613bc46eb
Kohsei Mochizuki <kose@KohseinoMacBook-Pro.local>
2025-11-21 10:10:27 +0900

    #22 feat:gamepkの入力して、analysis_dataを取得できるように

---
+++ loaders/gamepk_to_prob.py
@@ -0,0 +1,29 @@
+# gamepkを基に、ロジスティック回帰データを取得する
+
+import json
+import sys
+import os
+
+# プロジェクトルートをパスに追加
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from preprocess.data_processor import data_process
+from analysys.data_collection.data_processer import data_process_for_ra
+from analysys.data_collection.time_data_sellecting import time_data_sellecting
+from analysys.data_judge.catBoost_info_jufge import catBoost_info_jufge
+
+def get_data(gamepk):
+    _,processed_data = data_process(gamepk)
+    
+    processed_data_ra = data_process_for_ra(processed_data,gamepk)
+    
+    molded_data = time_data_sellecting(processed_data_ra,gamepk)
+    
+    analysis_data = catBoost_info_jufge(molded_data)
+    
+    return analysis_data
+    
+if __name__ == "__main__":
+    # 778062
+    gamepk = input("Enter the gamepk: ")
+    get_data(gamepk)
